b8bbe0918f34fb8d42489a6b851c6036e6603a51
Pradeep Kumar T <t.pradeepkumar@gmail.com>
2016-05-11 11:24:52 +0530

    Refactor

--- EzEventManager/src/ezeventmanager/eventmanager/EventManagerImpl.java
+++ EzEventManager/src/ezeventmanager/eventmanager/EventManagerImpl.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package eventmanager;
+package ezeventmanager.eventmanager;
 
 import ezeventmanager.events.*;
 import ezeventmanager.user.*;
